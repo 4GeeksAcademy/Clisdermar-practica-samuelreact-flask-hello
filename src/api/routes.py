59eb8a0e5c3b086856601887789fe307f83718ce
clisdermar <clisdermar@gmail.com>
2024-03-28 03:15:29 +0000

    authentication

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,17 +6,45 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+#metodos copiado de pagi JWT flask python
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if email == None or password == None:
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    
+    user = User.query.filter_by(email=email).one_or_none()
 
-    return jsonify(response_body), 200
+    if user != None:
+
+        if password == user.password:
+           access_token = create_access_token(identity=email)
+           return jsonify(access_token= access_token)
+        else:
+            return jsonify({"msj": "Wrong password"}), 401
+    
+    return jsonify({"msj": "User not found"}),404
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(email=current_user, is_active = True).one_or_none()
+    if user != None:
+        return jsonify(user.serialize()), 200
+    
+    return jsonify({"msj" : "El usuario no esta activo,disculpe contacte a soporte"}), 200
